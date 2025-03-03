8c410b376dd3c78033b8b6ffd76b59013aee7720
Rankela Sai Sri Harsha <saisriharsha.r@gmail.com>
2025-03-03 22:54:46 +0530

    updated main.jsx

--- code-genie-frontend/src/components/Main.jsx
+++ code-genie-frontend/src/components/Main.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import MainNav from "./MainNav";
+import "../assets/styles/main.css";
 
 const Main = () => {
   return (
@@ -9,9 +10,7 @@ const Main = () => {
         <div className="container" style={{ background: "black", opacity: 0.9 }}>
           <p className="homep1">
             Welcome to <span className="homep">CodeGenie</span>
-            
           </p>
-          
         </div>
       </div>
     </div>
