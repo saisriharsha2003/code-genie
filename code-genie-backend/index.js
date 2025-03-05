34ef03a1861a7d900b6e37fea5ac628e6d836be1
Rankela Sai Sri Harsha <saisriharsha.r@gmail.com>
2025-03-05 18:45:44 +0530

    modified backend import issues

--- code-genie-backend/index.js
+++ code-genie-backend/index.js
@@ -1,33 +1,30 @@
-const express = require("express");
-const mongoose = require("mongoose");
-require("dotenv").config();
-const cors = require("cors");
-const app = express();
-import { MONGODB_URL, JWT_SECRET } from "../config.js";
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import { MONGODB_URL } from "./config.js";
+import userRouter from "./routes/Routes.js"; 
 
+const app = express();
 const port = process.env.PORT || 8080;
 
 app.use(express.json());
 app.use(cors());
 
-const userRouter = require("./routes/Routes");
-
 const mongoURI = MONGODB_URL;
-mongoose.connect(mongoURI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-
-.then(() => {
-  console.log("Connected to MongoDB Atlas!");
-})
-
-.catch((error) => {
-  console.error("Error connecting to MongoDB Atlas:", error);
-});
+mongoose
+  .connect(mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to MongoDB Atlas!");
+  })
+  .catch((error) => {
+    console.error("Error connecting to MongoDB Atlas:", error);
+  });
 
 app.use("/api/user", userRouter);
 
 const server = app.listen(port, "0.0.0.0", () => {
-  console.log("Server started on port "+port);
+  console.log("Server started on port " + port);
 });
--- code-genie-backend/routes/Routes.js
+++ code-genie-backend/routes/Routes.js
@@ -1,16 +1,17 @@
-const express = require('express');
-const { signup, signin, getUserProfile, updatePassword, updateUserProfile, chat_response } = require('../controllers/userController');
+import express from "express";
+import { signup, signin, getUserProfile, updatePassword, updateUserProfile, chat_response } from "../controllers/userController.js"; // Use import instead of require
+
 const router = express.Router();
 
-router.route('/').get((req, res) => {
-    res.send('Welcome to the NoteNexus!');
+router.route("/").get((req, res) => {
+  res.send("Welcome to the NoteNexus!");
 });
 
-router.route('/register').post(signup);
-router.route('/login').post(signin);
-router.route('/chat').post(chat_response);
-router.route('/profile/:uname').get(getUserProfile);
-router.route('/update-password/:uname').put(updatePassword);
-router.route('/update-profile/:uname').put(updateUserProfile);
+router.route("/register").post(signup);
+router.route("/login").post(signin);
+router.route("/chat").post(chat_response);
+router.route("/profile/:uname").get(getUserProfile);
+router.route("/update-password/:uname").put(updatePassword);
+router.route("/update-profile/:uname").put(updateUserProfile);
 
-module.exports = router;
+export default router;
